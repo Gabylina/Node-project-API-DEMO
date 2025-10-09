da1ed057a6bec626dfc20ad605d8162fbf57b9a3
Gabylina <127333431+Gabylina@users.noreply.github.com>
2025-10-09 15:09:58 -0300

    migrate(file): src/events/ProjectCreated.js [skip ci]

---
+++ src/events/ProjectCreated.js
@@ -0,0 +1,9 @@
+// migrated from app/Events/ProjectCreated.php
+export class ProjectCreated {
+  static event='ProjectCreated';
+  constructor(payload={}){
+    Object.assign(this,payload);
+  }
+}
+
+export default { ProjectCreated };
